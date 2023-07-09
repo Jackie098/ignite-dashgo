6e9125e9b02471a763a9c1df12991b42e77b0728
Carlos Augusto <carlosmiranda19122@gmail.com>
2023-07-09 18:54:08 -0300

    style: added responsiviness in Header component

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,14 +1,20 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData: Boolean;
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
   return (
     <Flex align={"center"}>
-      <Box mr="4" textAlign={"right"}>
-        <Text>Carlos Augusto</Text>
-        <Text color={"gray.300"} fontSize={"small"}>
-          carlos.aug.developer@gmail.com
-        </Text>
-      </Box>
+      {showProfileData && (
+        <Box mr="4" textAlign={"right"}>
+          <Text>Carlos Augusto</Text>
+          <Text color={"gray.300"} fontSize={"small"}>
+            carlos.aug.developer@gmail.com
+          </Text>
+        </Box>
+      )}
 
       <Avatar
         size="md"
